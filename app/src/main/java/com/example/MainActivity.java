57d622b55a0f6d1da736200cc0a033f5bdbaa576
muhamed-hassan <mohamed.hassan.work@gmail.com>
2024-05-04 11:19:19 +0300

    update README.md

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -2,12 +2,35 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private TextView textView;
+
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text_view);
+        editText = (EditText) findViewById(R.id.edit_view);
+
+        Button helloButton = (Button) findViewById(R.id.hello_button);
+        helloButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String name = editText.getText().toString();
+                textView.setText("Hello, " + name + "!");
+            }
+        });
     }
 }
